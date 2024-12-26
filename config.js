2224bbdba5d08ee543d655854df926a3b7210fe3
Shaliya56 <161951978+Shaliya56@users.noreply.github.com>
2024-12-26 17:33:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "eyYhnZzC#K6RfMVaytP5RvHYO1r9mIq0lMpqcI4tr5_NspaMTG4A", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "yywzhTQZ#UMOdA8V8y8WVmpl_vEBwQx-UDbFoyV0YgNXAqcm3xSE", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
